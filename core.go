6dc0d5c98ebbca2a12114121a90cc8fa003ffe52
Sheritz Sogbesan <ssogbesan@gmail.com>
2025-02-26 15:38:21 -0500

    implement putHandler

--- core.go
+++ core.go
@@ -1,4 +1,12 @@
-func main
+package main
+
+import (
+	"errors"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+)
 
 var store = make(map[string]string)
 var ErrorNoSuchKey = errors.New("no such key")
@@ -25,3 +33,40 @@ func Delete(key string) error {
 	return nil
 }
 
+// putHandler expects to be called with a PUT request for the
+// "v1/key/{key}" resource
+
+func putHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	defer r.Body.Close()
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Register putHandler as the handler function for PUT requests matching
+	// "v1/key/{key}"
+	r.HandleFunc("/v1/key/{key}", putHandler).Methods("PUT")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
