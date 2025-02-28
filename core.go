fd1cbfeda52504ba74f610006ef25b31379651c1
Sheritz Sogbesan <ssogbesan@gmail.com>
2025-02-27 22:31:43 -0500

    move handlers and logger initialization to service.go

--- core.go
+++ core.go
@@ -4,10 +4,6 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"github.com/gorilla/mux"
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"sync"
 )
@@ -49,51 +45,6 @@ func Delete(key string) error {
 	delete(store.m, key)
 
 	return nil
-}
-
-// putHandler expects to be called with a PUT request for the
-// "v1/key/{key}" resource
-
-func putHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["key"]
-
-	value, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w,
-			err.Error(),
-			http.StatusInternalServerError)
-		return
-	}
-
-	defer r.Body.Close()
-
-	err = Put(key, string(value))
-	if err != nil {
-		http.Error(w,
-			err.Error(),
-			http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-}
-
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["key"]
-
-	value, err := Get(key)
-	if errors.Is(err, ErrorNoSuchKey) {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprint(w, value) // Write the value to the response
 }
 
 // Logging
@@ -216,49 +167,3 @@ const (
 	EventDelete EventType = iota
 	EventPut
 )
-
-// Initialize FileTransactionLogger
-
-var logger TransactionLogger
-
-func initializeTransactionLog() error {
-	var err error
-
-	logger, err = NewFileTransactionLogger("transaction.log")
-	if err != nil {
-		return fmt.Errorf("failed to create event logger: %w", err)
-	}
-
-	events, errors := logger.ReadEvents()
-
-	e := Event{}
-	ok := true
-
-	for ok && err == nil {
-		select {
-		case err, ok = <-errors: // Retrieve any errors; ok = false if channel has
-		case e, ok = <-events: // been closed
-			switch e.EventType {
-			case EventDelete:
-				err = Delete(e.Key)
-			case EventPut:
-				err = Put(e.Key, e.Value)
-			}
-		}
-	}
-
-	logger.Run()
-
-	return err
-}
-
-func main() {
-	r := mux.NewRouter()
-
-	// Register putHandler as the handler function for PUT requests matching
-	// "v1/key/{key}"
-	r.HandleFunc("/v1/key/{key}", putHandler).Methods("PUT")
-	r.HandleFunc("/v1/key/{key}", getHandler).Methods("GET")
-
-	log.Fatal(http.ListenAndServe(":8080", r))
-}
---
+++ service.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+)
+
+// putHandler expects to be called with a PUT request for the
+// "v1/key/{key}" resource
+
+func putHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	defer r.Body.Close()
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w,
+			err.Error(),
+			http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+
+	value, err := Get(key)
+	if errors.Is(err, ErrorNoSuchKey) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprint(w, value) // Write the value to the response
+}
+
+// Initialize FileTransactionLogger
+
+var logger TransactionLogger
+
+func initializeTransactionLog() error {
+	var err error
+
+	logger, err = NewFileTransactionLogger("transaction.log")
+	if err != nil {
+		return fmt.Errorf("failed to create event logger: %w", err)
+	}
+
+	events, errors := logger.ReadEvents()
+
+	e := Event{}
+	ok := true
+
+	for ok && err == nil {
+		select {
+		case err, ok = <-errors: // Retrieve any errors; ok = false if channel has
+		case e, ok = <-events: // been closed
+			switch e.EventType {
+			case EventDelete:
+				err = Delete(e.Key)
+			case EventPut:
+				err = Put(e.Key, e.Value)
+			}
+		}
+	}
+
+	logger.Run()
+
+	return err
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	// Register putHandler as the handler function for PUT requests matching
+	// "v1/key/{key}"
+	r.HandleFunc("/v1/key/{key}", putHandler).Methods("PUT")
+	r.HandleFunc("/v1/key/{key}", getHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
